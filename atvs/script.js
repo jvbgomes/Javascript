e773ed25e982092dd482453899de06bf302ba978
jvbgomes <jvbg16@gmail.com>
2026-03-19 03:36:27 -0300

    início de javascript

---
+++ atvs/script.js
@@ -0,0 +1,11 @@
+idade = 20;
+
+dataAniversario = "16 de junho";
+
+console.log("Olá, mundo!");
+console.log("Minha idade é: ", idade);
+console.log("Data do aniversário: ", dataAniversario);
+
+idade = 21;
+
+console.log("Minha nova idade depois do aniversário é: ", idade);
