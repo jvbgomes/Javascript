9335be9a2bf289c87eec112892bd8612ecbc3b67
jvbgomes <jvbg16@gmail.com>
2026-03-22 15:05:52 -0300

    dif entre var,let e const

--- atvs/script.js
+++ atvs/script.js
@@ -1,11 +1,27 @@
-idade = 20;
+// testando comentarios
 
-dataAniversario = "16 de junho";
 
-console.log("Olá, mundo!");
+//formas diferentes de criar variáveis
+//1 - varivel global
+//2 - var 
+//3 - let (mais atual, mais utilizada)
+//4 - const (varivael constante, ou seja, não pode ser alterada depois de criada)
+
+
+//variável que armazena a idade(variavel global, quase nunca faz assim.)
+let idade = 20;
+
+//a data que eu faço aniversário
+const dataAniversario = "16 de junho";
+
+//imprimindo a minha idade atual no console
 console.log("Minha idade é: ", idade);
+
+//imprimindo a minha data de aniversário no console
 console.log("Data do aniversário: ", dataAniversario);
 
+//idade depois do aniversário
 idade = 21;
 
+//imprimindo minha idade atual novamente
 console.log("Minha nova idade depois do aniversário é: ", idade);
