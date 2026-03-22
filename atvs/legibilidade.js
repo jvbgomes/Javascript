644c7f6d7dae85ccbf525fe8c3d576b34e0a096b
jvbgomes <jvbg16@gmail.com>
2026-03-22 15:40:06 -0300

    legibilidade de variaveis

---
+++ atvs/legibilidade.js
@@ -0,0 +1,24 @@
+let alturaDoVinny = 1.89;
+
+console.log(alturaDoVinny);
+
+let altura = 7;
+let largura = 5;
+let area = 35;
+
+//letras maiusculas e minusculas fazem diferenca
+
+let preco = 10;
+let Preco = 20;
+
+console.log("preco:", preco);
+console.log("Preco:", Preco);
+//console.log("precO: ", precO); //precO is not defined
+
+let IdadeUsuario = 20;
+let nomeCompleto = "João Victor";
+
+//nao sabemos que nao se trata
+
+let a = 1;
+let b = false;
