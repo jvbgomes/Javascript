7d823960909ab9802cde7bc9d0de40a3b625fcb0
jvbgomes <jvbg16@gmail.com>
2026-03-22 15:23:25 -0300

    entendendo formas de erros

---
+++ atvs/hoisting.js
@@ -0,0 +1,7 @@
+//console.log("Minha idade atual é: ", idade);
+
+let idade = 20;
+//variável nao pode ser referencia antes da declaração
+//var pode 
+//let, const não podem ser referenciados antes da inicialização
+console.log("Minha idade atual depois de declarar: ", idade);
