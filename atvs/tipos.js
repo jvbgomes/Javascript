e36257f5ac750cb8d97d67bffaf376d7f65c6ad5
jvbgomes <jvbg16@gmail.com>
2026-03-22 15:33:05 -0300

    tipos de variaveis

---
+++ atvs/tipos.js
@@ -0,0 +1,23 @@
+let nome = "João";
+let idade = 20;
+let altura = 1.86;
+
+console.log(typeof nome);  //string
+console.log(typeof idade); //number
+console.log(typeof altura); //number
+
+let estaChovendo = true; //true ou false
+
+console.log(typeof estaChovendo); //boolean
+
+let cidade;
+
+console.log(typeof cidade); //undefined
+
+let jaAprendeuJS = null;
+
+console.log(typeof jaAprendeuJS); //object, de fato é null(vazio)
+
+let simbolo = Symbol();
+
+console.log(typeof simbolo); //symbol
