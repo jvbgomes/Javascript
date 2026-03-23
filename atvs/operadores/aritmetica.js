464e427f0d31f06e2f0f944c35e0481a61a14642
jvbgomes <jvbg16@gmail.com>
2026-03-23 02:52:31 -0300

    operadores matematicos

---
+++ atvs/operadores/aritmetica.js
@@ -0,0 +1,50 @@
+const numeroUm = 4;
+const numeroDois = 8;
+
+const subtracao = numeroUm - numeroDois;
+
+console.log("Subtração: ", subtracao);
+
+const soma = numeroUm + numeroDois;
+
+console.log("Soma: ", soma);
+
+const multiplicacao = numeroUm * numeroDois;
+
+console.log("Multiplicação: ", multiplicacao); 
+
+const divisao = numeroUm / numeroDois;
+
+console.log("Divisão: ", divisao);
+
+const contaComplexa = numeroUm + (numeroDois * numeroDois);
+
+console.log("Conta Complexa: ", contaComplexa);
+
+const resto = numeroDois % numeroUm;
+
+console.log("Resto da divisão: ", resto);
+
+const potencia = numeroUm ** 2;
+
+console.log("Potência: ", potencia);
+
+let contador = 5;
+
+//contador = contador + 1;
+contador++;
+console.log("Incremento: ", contador);
+
+//contador = contador - 1;
+contador--;
+console.log("Decremento: ", contador);
+
+let numTres = 6;
+
+//numTres = numTres * 2;
+numTres *= 3;
+console.log(numTres);
+
+//numTres = numTres / 2;
+numTres = numTres / 2;
+console.log(numTres);
