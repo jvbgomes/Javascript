bbb1089bbc63d71ddcdc8c3401d932edbbd52ab7
jvbgomes <jvbg16@gmail.com>
2026-03-23 15:41:21 -0300

    condicionais

---
+++ conteudo/condicoes/condicionais.js
@@ -0,0 +1,21 @@
+const idade = 17;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+
+const notaDoAluno = 8;
+
+if (notaDoAluno >= 9) {
+    console.log("Excelente!");
+}
+else if (notaDoAluno >= 7 && notaDoAluno <= 8) {
+    console.log("Bom!");
+}
+else if (notaDoAluno >= 4 && notaDoAluno <= 6) {
+    console.log("Médio!");
+} else {
+    console.log("Ruim!");
+}
