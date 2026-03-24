8e690c22dcbd8b21149d83dd1a50ff266e4f253d
jvbgomes <jvbg16@gmail.com>
2026-03-24 20:38:54 -0300

    while

---
+++ conteudo/repeticoes/while.js
@@ -0,0 +1,15 @@
+let contador = 1;
+
+while (contador <= 10) {
+    console.log("Número atual: ", contador);
+    contador++;
+}
+
+console.log("------------------------------");
+
+let numero = 1;
+
+do {
+    console.log("Número atual: ", numero);
+    numero++;
+} while (numero <= 10);
