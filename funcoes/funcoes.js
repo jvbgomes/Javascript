f441d4782955eed291582244e1485a9a1a54da1d
João Victor <jvbg16@gmail.com>
2026-03-25 15:36:07 -0300

    functions

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,3 +1,23 @@
-function saudcao(nome) {
-    console.log('Olá, ', nome);
-}
+//function saudacao(nome) { //nome é um parametro da função
+//    console.log('Olá, ', nome);
+//}
+
+const saudacao = nome => console.log('Vida longa e prospera,', nome) //Função de seta (arrow function) - forma mais curta de escrever uma função, sem a palavra "function" e sem chaves, quando a função tem apenas uma linha de código.
+
+saudacao('João') //'João' é o "argumento"
+saudacao('Maria') //'Maria' é o "argumento"
+saudacao('Pedro') //'Pedro' é o "argumento"
+
+//function calcularDobroDeUm(numero) {
+//    return numero * 2;
+//}
+
+//const calcularDobroDeUm = (numero) => {
+//    return numero * 2;
+//}
+
+const calcularDobroDeUm = numero => numero * 2; //forma mais curta de escrever a função, sem a palavra "return" e sem chaves, quando a função tem apenas uma linha de código.
+
+console.log(calcularDobroDeUm(5)) //10
+
+console.log('oi', 'tudo', 'bem', '?')
