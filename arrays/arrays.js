88a07cac4cf033be08a0194964bf0815458d1df5
João Victor <jvbg16@gmail.com>
2026-03-29 03:17:27 -0300

    arrays com for

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -3,15 +3,42 @@ const listaDeNomes = ['João', 'Maria', 'Pedro', 'Ana']
 
 const frutas = ['Uva', 'Banana', 'Kiwi', 'Maça', 'Morango']
 
-console.log('A lista é:',frutas)
+// console.log('A lista é:',frutas)
 
-console.log('Primeira fruta:',frutas[0])
-console.log('Última fruta:', frutas[4])
+// console.log('Primeira fruta:',frutas[0])
+// console.log('Última fruta:', frutas[4])
 
-console.log('Total de frutas:', frutas.length)
+// console.log('Total de frutas:', frutas.length)
 
-frutas.push('Melancia')
+// frutas.push('Melancia')
 
-console.log('Total de frutas depois de adicionar melancia:', frutas.length)
+// console.log('Total de frutas depois de adicionar melancia:', frutas.length)
 
-console.log('Ultima fruta: ', frutas[frutas.length - 1])
+// console.log('Ultima fruta: ', frutas[frutas.length - 1])
+
+//frutas.splice(2, 1)
+
+//console.log('Depois de remover: ', frutas)
+
+console.log("\n")
+
+for (let i = 0; i < frutas.length; i++){
+    console.log('índice: ', i)
+    console.log(frutas[i])
+}
+
+console.log("\n")
+
+console.log('Usando forEach')
+
+frutas.forEach((valor, indice) => {
+    console.log('índice: ', indice, valor)
+})
+
+console.log("\n")
+
+console.log('Usando for of:')
+
+for (const fruta of frutas) {
+    console.log('fruta da vez: ', fruta)
+}
