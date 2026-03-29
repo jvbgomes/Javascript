51260fea7195bb46a06ef1fccc022ff61c793c49
João Victor <jvbg16@gmail.com>
2026-03-29 03:59:30 -0300

    lista 6 de arrays

---
+++ arrays/lista6/qsts6.js
@@ -0,0 +1,42 @@
+const nomes = ['João', 'Mikaelly', 'Carlos', 'Breno', 'Pretinha']
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log('índice: ', i)
+    console.log(nomes[i])
+}
+
+console.log("\n")
+
+const frutas = ['Kiwi', 'Banana', 'Melancia', 'Maça', 'Pêra']
+console.log('Lista de frutas: ', frutas)
+
+frutas.push('Uva') // adiciona um elemento no final da lista
+frutas.shift() // remove o primeiro elemento da lista
+console.log("\n")
+
+console.log('Lista nova de frutas: ', frutas)
+
+const cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Salvador']
+
+console.log('Quantidade de cidades: ', cidades.length)
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let soma = 0
+for (let i =  0; i < numeros.length; i++) {
+    soma += numeros[i]
+}
+console.log('Soma dos números: ', soma)
+
+const notas = [7, 8, 9, 6]
+
+let somaNotas = 0
+let mediaNotas = 0
+
+for (let i = 0; i < notas.length; i++) {
+    somaNotas += notas[i]
+}
+mediaNotas = somaNotas / notas.length
+
+console.log('Média das notas: ', mediaNotas)
+
