c6157a906459f9b93d1f05ba9bca9d45ed345fc1
João Victor <jvbg16@gmail.com>
2026-04-01 01:27:08 -0300

    lista 7 objetos

--- objetos/lista7/listaobjetos.js
+++ objetos/lista7/listaobjetos.js
@@ -3,7 +3,53 @@ const pessoa = {
     idade: 21,
     profissao: 'Desenvolvedor',
 }
-console.log(pessoa.nome);
-console.log(pessoa.idade);
-console.log(pessoa.profissao);
+console.log('Nome: ',pessoa.nome);
+console.log('Idade: ',pessoa.idade);
+console.log('Profissão: ',pessoa.profissao);
+
+console.log('--------------------------------------------');
+
+pessoa.idade = 22;
+console.log('Idade atualizada: ', pessoa.idade);
+
+pessoa.cidade = 'Natal-RN';
+console.log('Cidade: ', pessoa.cidade);
+
+console.log('--------------------------------------------');
+
+function criarPessoa(nome, idade, profissao) {
+    return newpessoa = {
+        nome: nome,
+        idade: idade,
+        profissao: profissao
+    }
+}
+console.log(criarPessoa('Maria', 30, 'Designer'));
+
+console.log('--------------------------------------------');
+
+const lista = [
+    pessoa1 = {
+        nome: 'Ana',
+        idade: 18
+    },
+    pessoa2 = {
+        nome: 'Carlos',
+        idade: 17
+    },
+    pessoa3 = {
+        nome: 'Beatriz',
+        idade: 22
+    }
+]
+console.log(lista);
+
+for (const pessoa of lista) {
+    if (pessoa.idade >= 18) {
+        console.log(pessoa.nome, 'é maior de idade');
+    }
+}
+
+console.log('--------------------------------------------');
+
 
