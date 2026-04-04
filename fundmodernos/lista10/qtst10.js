1e43b627b3659682169b02244abfd1304ba7a5a3
João Victor <jvbg16@gmail.com>
2026-04-04 16:24:27 -0300

    atualização da lista 10 js]

---
+++ fundmodernos/lista10/qtst10.js
@@ -0,0 +1,83 @@
+let pessoa = {
+    nome: 'João Victor',
+    idade: 20,
+    email: 'jvbg16@gmail.com'
+};
+
+console.log(pessoa);
+
+let {nome, idade, email} = pessoa;
+
+console.log(nome);
+console.log(idade);
+console.log(email);
+console.log('\n');
+console.log('-----------------------------\n');
+
+let ling = ['JavaScript', 'Python', 'Java'];
+
+console.log(ling);
+
+let [ling1, ling2, ling3] = ling;
+
+console.log(ling1);
+console.log(ling2);
+console.log(ling3);
+
+console.log('\n');
+console.log('-----------------------------\n');
+
+function restoperator(...numeros) {
+    let total = 0
+
+    for (let numero of numeros) { // usando o for tradicional, temos for (let i = 0; i < numeros.length; i++) { total += numeros[i];}
+        total += numero;
+    }
+
+    return total
+}
+
+console.log(`O total é: ${restoperator(6, 7, 54, 12, 34, 23)}`);
+
+console.log('\n');
+console.log('-----------------------------\n');
+
+let frutas1 = ['Maçã', 'Banana', 'Laranja'];
+console.log(frutas1);
+let frutas2 = ['Abacaxi', 'Manga', 'Uva'];
+console.log(frutas2);
+
+let todasFrutas = [...frutas1, ...frutas2];
+console.log(todasFrutas);
+
+console.log('\n');
+console.log('-----------------------------\n');
+
+let person1 = {
+    name: 'Alice'
+};
+
+let person2 = {
+    age: 27
+};
+
+let sumperson = {
+    ...person1,
+    ...person2
+};
+
+console.log(sumperson);
+
+console.log('\n');
+console.log('-----------------------------\n');
+
+function saudacao(nome) {
+    if (nome !== undefined) {
+        return `Olá, ${nome}`;
+    } else {
+        return 'Olá, visitante';
+    }
+}
+
+console.log(saudacao('João'));
+console.log(saudacao());
