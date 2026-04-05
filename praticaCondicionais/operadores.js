246246d9ee7ffa5bbd2c15bd9cd4fb83945789cb
João Victor Bezerra Gomes <jvbg16@gmail.com>
2026-04-05 02:20:56 -0300

    questoes de operadores

---
+++ praticaCondicionais/operadores.js
@@ -0,0 +1,18 @@
+// verificar se um ano é bissexto
+// um ano é bissexto se:
+// deve ser divisível por 4 mas NÃO divisível por 100
+// OU
+// deve ser divisível por 100 e por 400
+
+//const ano = 2000;
+//const ano = 2022;
+//const ano = 1992;
+
+const ano = 2000;
+
+if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
+    console.log(`${ano} é um ano bissexto.`);
+} else {
+    console.log(`${ano} não é um ano bissexto.`);
+}
+
