f9bf6990c0830569e4d0b8113698c3638148bc67
João Victor Bezerra Gomes <jvbg16@gmail.com>
2026-04-06 09:57:29 -0300

    qsts ...

--- praticaCondicionais/qstscond.js
+++ praticaCondicionais/qstscond.js
@@ -26,3 +26,18 @@ if (temp > 25) {
     console.log('Temperatura inválida');
 }
 
+//Se a pessoa tiver menos de 12 anos, ela deve ser classificada como "Infantil".
+//Se a pessoa tiver 12 anos ou mais, mas menos de 18 anos, ela deve ser classificada como "Juvenil".
+//Se a pessoa tiver 18 anos ou mais, ela deve ser classificada como "Adulto".
+
+const idadePessoa = 34;
+
+if (idadePessoa >= 18) {
+    console.log('Adulto');
+} else if (idadePessoa >= 12 && idadePessoa < 18) {
+    console.log('Juvenil');
+} else if (idadePessoa < 12) {
+    console.log('Infantil');
+} else {
+    console.log('Idade inválida');
+}
