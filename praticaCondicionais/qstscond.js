92f1e8cbad730774d185a8f171202cc8ce6789d6
João Victor Bezerra Gomes <jvbg16@gmail.com>
2026-04-06 09:53:10 -0300

    qsts...

--- praticaCondicionais/qstscond.js
+++ praticaCondicionais/qstscond.js
@@ -8,3 +8,21 @@ if (idade >= 18) {
     console.log('Idade inválida.');
 }
 
+// Menor que 15: "Frio"
+// De 15 a 25: "Agradável"
+// Acima de 25: "Quente"
+
+const temp = -12;
+
+if (temp > 25) {
+    console.log('Quente');
+} else if (temp >= 15 && temp <= 25) {
+    console.log('Agradável');
+} else if (temp < 15) {
+    console.log('Frio');
+} else if (temp < -20) {
+    console.log('Temperatura extremamente baixa');
+} else {
+    console.log('Temperatura inválida');
+}
+
