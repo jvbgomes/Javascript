e50fd80d417c3169b57d117fa76624a89c0812b2
João Victor Bezerra Gomes <jvbg16@gmail.com>
2026-04-06 09:49:16 -0300

    qst de condicionais

---
+++ praticaCondicionais/qstscond.js
@@ -0,0 +1,10 @@
+const idade = 21;
+
+if (idade >= 18) {
+    console.log('Acesso permitido.');
+} else if (idade < 18) {
+    console.log('Acesso negado. Você é menor de idade.');
+} else {
+    console.log('Idade inválida.');
+}
+
