2a15214b2d49d7cf0793d5ef9cadbe5db7ab37e0
João Victor Bezerra Gomes <jvbg16@gmail.com>
2026-04-07 13:39:11 -0300

    Questoes de laços

--- praticaLoops/tipos.js
+++ praticaLoops/tipos.js
@@ -33,3 +33,69 @@ for (let i = 1; i <=30; i++) {
 }
 console.log(`O contador foi incrementado ${contador} vezes!`);
 
+console.log('\n')
+console.log('----------------------------------------------------------------')
+// Revisando While e do-while
+
+let numeroSecreto = 8;
+let numeroAleat = 0;
+let count = 0;
+
+while (numeroAleat !== numeroSecreto) {
+    numeroAleat = Math.floor(Math.random() * 50) + 1;
+    count++;
+}
+console.log(`Adivinhou o numero secreto em ${count} tentativas!`);
+
+let numeroRandom = 0;
+do {
+    numeroRandom = Math.floor(Math.random() * 50) + 1
+}while (numeroRandom % 2 !== 0);
+
+console.log(`O número ${numeroRandom} é divisível por 2!`);
+
+console.log('\n')
+console.log('----------------------------------------------------------------')
+
+// Propriedade .Length
+
+const palavra = 'papagaio';
+
+// 0 1 2 3 4 5 6 7
+//["p","a","p","a","g","a","i","o"];
+
+for (let i = 0; i < palavra.length; i++) {
+    console.log(palavra[i]);
+}
+
+//Praticando while e do-while
+
+//gerar um laco que tente "adivinhar" um dado numero de 1 a 50
+// e conte a quantidade de tentativas ate o resultado 
+
+console.log('\n')
+console.log('----------------------------------------------------------------')
+
+const numbSecret = 12;
+let numbRandom = 0;
+let countRandom = 0;
+
+while (numbRandom !== numbSecret) {
+    numbRandom = Math.floor(Math.random() * 50) + 1;
+    countRandom++;
+}
+console.log(`O numero secreto foi adivinhado em ${countRandom} tentativas!`);
+
+console.log('\n')
+console.log('----------------------------------------------------------------')
+
+//criar um laco que gere um numero aleatorio de 1 a 50
+//executar o laco enquanto o numero gerado for par
+
+let numbRandom2 = 0;
+
+do {
+    numbRandom2 = Math.floor(Math.random() * 50) + 1;
+} while (numbRandom2 % 2 !== 0);
+
+console.log(`O numero ${numbRandom2} é par!`);
