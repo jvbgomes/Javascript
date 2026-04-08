9474149fa1b96755ecada52f0d5e15e0c91f95d3
João Victor <jvbg16@gmail.com>
2026-04-08 00:36:49 -0300

    Qsts de loops

--- praticaLoops/qstsloops.js
+++ praticaLoops/qstsloops.js
@@ -37,10 +37,28 @@ console.log('----------------------------------------------------------------')
 
 const entradas = ["Ana", "Bruno", "Carla", "fim", "Eduardo"];
 
-for (let i = 0; i < entradas.length; i++) {
+//for (let i = 0; i < entradas.length; i++) {
+//    console.log(entradas[i]);
+//    if(entradas[i].toLowerCase() === "fim") {
+//        console.log("Encerrando o loop...");
+//        break;
+//    }
+//}
+
+let i = 0;
+while (entradas[i] !== "fim") {
     console.log(entradas[i]);
-    if(entradas[i].toLowerCase() === "fim") {
-        console.log("Encerrando o loop...");
+    i++;
+}
+
+console.log('\n')
+console.log('----------------------------------------------------------------')
+
+for (let i = 1; i <= 20; i++) {
+    if (i === 10) {
+        console.log("Número proibido encontrado! Encerrando...");
         break;
     }
+    console.log(i);
 }
+
