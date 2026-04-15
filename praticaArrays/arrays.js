621676299528a2457c8d85f9fe52e1615a2ac99c
jvbgomes <jvbg16@gmail.com>
2026-04-15 12:58:25 -0300

    arrays em js

---
+++ praticaArrays/arrays.js
@@ -0,0 +1,21 @@
+//const arrNumeros = [1, 2, 3, 4, 5];
+//
+//const arrMisto = [5, true, 'texto'];
+//
+//const arrArrays = [[1, 2], ['a', 'b', 'c'], [true, false]];
+//
+//const arrObjetos = [{a: 1}, {b: 2}, {c: 3}];
+
+//ARRAY
+
+const arr = [1, 2, 3, 4];
+
+console.log(arr[0]); 
+console.log(arr[1]); 
+console.log(arr[2]); 
+console.log(arr[3]); 
+
+arr[0] = 3;
+arr[5] = 10;
+
+console.log(arr);
