6ead4bdc329e50a9605b268ff81351b85ac2f94e
João Victor <jvbg16@gmail.com>
2026-04-15 23:38:13 -0300

    spread e slice

--- praticaArrays/qstsArrays/clonandoArray.js
+++ praticaArrays/qstsArrays/clonandoArray.js
@@ -16,3 +16,21 @@ copia.push('Relógio');
 copia.push('Óculos');
 
 console.log(copia);
+
+//OU POSSO FAZER ASSIM:
+
+const copia2 = pedidos.slice();
+
+copia2.push('Carteira');
+copia2.push('Cinto');
+
+console.log(copia2);
+
+//OU USANDO O SPREAD OPERATOR:
+
+const copia3 = [...pedidos];
+
+copia3.push('Celular');
+copia3.push('Carregador');
+
+console.log(copia3);
