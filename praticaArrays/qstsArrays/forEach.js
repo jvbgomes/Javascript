249e05dd7f867c9178c17d4ad1203cc7568847df
João Victor <jvbg16@gmail.com>
2026-04-15 23:13:43 -0300

    qts com tipos de fors js

---
+++ praticaArrays/qstsArrays/forEach.js
@@ -0,0 +1,5 @@
+const mensagens = ['Pedido entregue', 'Pedido em trânsito', 'Pedido recebido'];
+
+mensagens.forEach((mensagem) => {
+    console.log('Notificação: ' + mensagem);
+});
---
+++ praticaArrays/qstsArrays/forOf.js
@@ -0,0 +1,5 @@
+const estudantes = ['João', 'Maria', 'Pedro', 'Ana'];
+
+for (let estudante of estudantes) {
+    console.log('Estudante: ' + estudante);
+}
