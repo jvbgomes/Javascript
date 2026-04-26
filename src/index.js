bb03144ae58ebb466123c6e324f877344850c378
jvbgomes <jvbg16@gmail.com>
2026-04-26 18:11:02 -0300

    feat: pega as break lines e separa-as em arrays de objetos

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ const link = arquivePath[2];
 //});
 
 fs.readFile(link, 'utf-8', (err, data) => {
-    duplicatedWords(data);
+    breakLines(data);
+    //duplicatedWords(data);
 });
 
 //criar um array com as palavras
 //contar as ocorrencias
 //montar um objeto com o resultado
+
+function breakLines(data) {
+    const lines = data.toLowerCase().split('\n');
+    const count = lines.map(lines => {
+        return duplicatedWords(lines);
+    })
+    console.log(count);
+}
 
 function duplicatedWords(data) {
     const wordlist = data.split(' '); //divide o texto em palavras usando o espaco como separador;
@@ -23,7 +32,7 @@ function duplicatedWords(data) {
     wordlist.forEach(word => {
         result[word] = (result[word] || 0) + 1; //se a palavra ja existir no objeto, pega o valor atual e soma 1, se nao, cria um novo com 0.
     })
-    console.log(result);
+    return result;
 }
 
 // process.argv é um array que contém os argumentos passados para o processo Node.js. O primeiro elemento é o caminho para o executável do Node.js, o segundo elemento é o caminho para o arquivo JavaScript que está sendo executado, e os elementos subsequentes são os argumentos adicionais passados para o script.
