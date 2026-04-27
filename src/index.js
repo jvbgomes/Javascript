ca0d243aad4ac03bc209567d79bf8653555852f7
jvbgomes <jvbg16@gmail.com>
2026-04-27 03:00:38 -0300

    fix: correção de um possivel retorno mesmo o valor sendo falsy, com nullish(??)

--- src/index.js
+++ src/index.js
@@ -33,16 +33,17 @@ function extractLines(data) {
 //expressao regular regex -> /[.,\/#!$%\^&\*;:{}=\-_`~()]/g -> remove os caracteres especiais do texto, deixando apenas as palavras. O 'g' no final indica que a substituição deve ser feita globalmente no texto.
 
 function cleanWords(word) {
-    return word.replace(/[/.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 }
 
 function duplicatedWords(data) {
-    const wordlist = data.split(' '); //divide o texto em palavras usando o espaco como separador;
+    const wordlist = data.split(' '); //divide o texto em palavras usando o espaco como separador.
     const result = {};
     wordlist.forEach(word => {
         if (word.length >= 3) {
             const cleanWord = cleanWords(word);
-            result[cleanWord] = (result[cleanWord] || 0) + 1; 
+            result[cleanWord] = (result[cleanWord] ?? 0) + 1; 
+            // ?? é um operador de coalescência nula que retorna o valor à direita (0) se o valor à esquerda for null ou undefined.
         }
         //se a palavra ja existir no objeto, pega o valor atual e soma 1, se nao, cria um novo com 0.
     })
