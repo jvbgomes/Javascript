29e882cc5d8f39b166d7b0f709d3fe3467f53850
João Victor <jvbg16@gmail.com>
2026-04-29 20:58:47 -0300

    feat(routes): add GET by id, PUT and DELETE routes for books

--- express-mongo/src/routes/booksRoutes.js
+++ express-mongo/src/routes/booksRoutes.js
@@ -4,5 +4,9 @@ import bookController from '../controllers/bookController.js';
 const routes = express.Router();
 
 routes.get("/books", bookController.getBooks);
+routes.get("/books/:id", bookController.getBookById);
+routes.post("/books", bookController.createBook);
+routes.put("/books/:id", bookController.updateBook);
+routes.delete("/books/:id", bookController.deleteBook);
 
 export default routes;
