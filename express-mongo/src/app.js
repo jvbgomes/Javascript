3de4569461de57cd8988e2f13587349fb5e87bd7
João Victor <jvbg16@gmail.com>
2026-04-30 03:04:03 -0300

    feat(app): register errorHandler middleware

--- express-mongo/src/app.js
+++ express-mongo/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDB from "./config/dbConnect.js";
+import errorHandler from "./middlewares/errorHandler.js";
 import routes from "./routes/index.js";
 
 const connection = await connectDB();
@@ -7,12 +8,13 @@ const connection = await connectDB();
 connection.on("error", (err) => {
     console.error("Connection error:", err);
 });
-
 connection.once("open", () => {
     console.log("Database connected");
 });
 
 const app = express();
 routes(app);
+
+app.use(errorHandler);
 
 export default app;
