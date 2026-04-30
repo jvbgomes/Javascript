96e47ac3584b86285cc71e13ae45aafdfe02932b
João Victor <jvbg16@gmail.com>
2026-04-29 23:19:02 -0300

    feat(routes): register authors routes in app

--- express-mongo/src/routes/index.js
+++ express-mongo/src/routes/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import books from './booksRoutes.js';
+import authors from './authorsRoutes.js';
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
         res.status(200).send('Curse of Node.js');
     });
 
-    app.use(express.json(), books); //Middleware para interpretar o corpo da requisição como JSON
+    app.use(express.json(), books, authors); //Middleware para interpretar o corpo da requisição como JSON
 };
 
 export default routes;
