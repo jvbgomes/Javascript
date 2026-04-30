df3e05738f8c890d08b2cd4056999af2f8db7301
João Victor <jvbg16@gmail.com>
2026-04-29 23:18:57 -0300

    refactor(routes): update import to match renamed BookController class

--- express-mongo/src/routes/booksRoutes.js
+++ express-mongo/src/routes/booksRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import bookController from '../controllers/bookController.js';
+import BookController from '../controllers/bookController.js';
 
 const routes = express.Router();
 
-routes.get("/books", bookController.getBooks);
-routes.get("/books/:id", bookController.getBookById);
-routes.post("/books", bookController.createBook);
-routes.put("/books/:id", bookController.updateBook);
-routes.delete("/books/:id", bookController.deleteBook);
+routes.get("/books", BookController.getBooks);
+routes.get("/books/:id", BookController.getBookById);
+routes.post("/books", BookController.createBook);
+routes.put("/books/:id", BookController.updateBook);
+routes.delete("/books/:id", BookController.deleteBook);
 
 export default routes;
