af63713ff00e01c067177a0042112290dc1cde6d
João Victor <jvbg16@gmail.com>
2026-04-30 00:31:38 -0300

    style(routes): replace single quotes with double quotes in index

--- express-mongo/src/routes/index.js
+++ express-mongo/src/routes/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import books from './booksRoutes.js';
-import authors from './authorsRoutes.js';
+import express from "express";
+import books from "./booksRoutes.js";
+import authors from "./authorsRoutes.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => {
-        res.status(200).send('Curse of Node.js');
+        res.status(200).send("Curse of Node.js");
     });
 
     app.use(express.json(), books, authors); //Middleware para interpretar o corpo da requisição como JSON
