2b96023281bbe84d144cc9f8a2c469e30b443af0
João Victor <jvbg16@gmail.com>
2026-04-30 16:50:10 -0300

    fix(server): correct protocol in startup log message

--- express-mongo/server.js
+++ express-mongo/server.js
@@ -4,6 +4,6 @@ import app from "./src/app.js";
 const port =  process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta https://localhost:${port}`);
+    console.log(`Servidor rodando na porta http://localhost:${port}`);
 });
 
